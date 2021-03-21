ef1e54bc98b7e63a5146cb616928382136c4bdb8
GreatDestroyerCharlie <1669247240@qq.com>
2021-03-22 00:36:05 +0300

    create engine project and test project

---
+++ ConsoleGameEngine/ConsoleGameEngine/ConTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGameEngine
+{
+    public class ConTest
+    {
+        public static string BuildModeString()
+        {
+            string testString = string.Empty;
+#if DEBUG
+            testString += "DEBUG";
+#else
+            testString += "RELEASE";
+#endif
+            return testString;
+        }
+    }
+}
---
+++ ConsoleGameEngine/ConsoleGames/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using ConsoleGameEngine;
+
+namespace ConsoleGames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = ConTest.BuildModeString();
+            Console.WriteLine(str);
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
