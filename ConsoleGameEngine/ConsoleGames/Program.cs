a9dc8314a29346cc1f09bc4da266657bc88d5f88
GreatDestroyerCharlie <1669247240@qq.com>
2021-03-29 22:41:09 +0300

    update

--- ConsoleGameEngine/ConsoleGames/Program.cs
+++ ConsoleGameEngine/ConsoleGames/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 
 using ConsoleGameEngine;
-using ConsoleGameEngine.Include;
 
-using static ConsoleGameEngine.Include.NativeMethods;
+using Vanara.PInvoke;
+
+using static Vanara.PInvoke.Kernel32;
+using static Vanara.PInvoke.User32;
 
 namespace ConsoleGames
 {
@@ -11,26 +13,8 @@ namespace ConsoleGames
     {
         static void Main(string[] args)
         {
-            IntPtr hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-            CHAR_INFO[] charInfos = new CHAR_INFO[20];
-            for (int i = 0; i < charInfos.Length; i++)
-            {
-                charInfos[i].Attributes = 7;
-                charInfos[i].UnicodeChar = 'a';
-            }
-            SMALL_RECT rect = new SMALL_RECT() { Left = 0, Top = 0, Bottom = 1, Right = 9 };
-            bool s = WriteConsoleOutput(hOutput, charInfos,
-                new COORD() { X = 10, Y = 2 }, new COORD() { X = 0, Y = 0 }, ref rect);
-
-            bool suc = ConTest.LoadNativeDLL();
-            Console.WriteLine("Load Native DLL:" + suc);
-
-            string str = ConTest.BuildModeString();
-            Console.WriteLine(str);
-
-            Console.WriteLine("Hello World!");
-
-            Vanara.PInvoke.Kernel32.SetConsoleTitle("hello vanara!");
+            Console.WriteLine("Load Native DLL:" + ConTest.LoadNativeDLL());
+            Console.WriteLine("BuildMode:" + ConTest.BuildModeString());
 
             Audio.AudioPlayOneShot("..\\..\\..\\..\\..\\resources\\BOBACRI - Sleep Away.mp3");
 
